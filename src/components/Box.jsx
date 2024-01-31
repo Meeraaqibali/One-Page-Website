e77515317e14b183d56f079252761028b7191a74
Manika Luhano <luhano.manika123>
2024-01-31 21:11:04 +0500

    update

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-function Box() {
+
+function Box(props) {
   return (
-    <div>Box</div>
+    <div className='s-box'>
+        <div className="s-b-img">
+            <img src={props.image} alt="{props.alte}" />
+        </div>
+        <div className="s-b-text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Magnam doloremque dolores, excepturi voluptate earum perspiciatis modi necessitatibus pariatur nesciunt quae ipsam quod, quos porro tenetur! 
+                Praesentium dolorum quam inventore maiores?</p>
+                <a href="#" className="cv-btn1" id='btn'>{props.button}</a>
+                </div>
+    </div>
   )
 }
 
-export default Box
+export default Box;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Box from './Box';
+import image1 from '../images/s1.png'; // Corrected import statement
+import image2 from '../images/s2.png'; // Corrected import statement
 
 function Services() {
   return (
-    <div id='services' >
-         <div className="f-heading">
-      <h1>Features</h1>
-      <p>Here are some features of the Software</p>
-     </div>
-     <div className="b-container">
-        <Box/>
-     </div>
+    <div id='services'>
+      <div className="s-heading">
+        <h1>Services</h1>
+        <p>Here are some Services I provide</p>
+      </div>
+      <div className="b-container">
+        <Box image={image1} alt="image1" button="PYTHON" />
+        <Box image={image2} alt="image2" button="REACT" />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Services;
